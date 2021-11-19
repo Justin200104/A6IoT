fc5339060755c35aeaadf4ed1a39777333b2777a
Justin200104 <57876808+Justin200104@users.noreply.github.com>
2021-11-19 13:57:34 -0500

    added a bunch of code for the mqtt second app

--- A6Two/MainWindow.xaml.cs
+++ A6Two/MainWindow.xaml.cs
@@ -18,7 +18,7 @@ using System.Drawing;
 using uPLibrary.Networking.M2Mqtt;
 using uPLibrary.Networking.M2Mqtt.Messages;
 
-namespace A6
+namespace A6Two
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -34,18 +34,18 @@ namespace A6
 
         private void sendButton_Click(object sender, RoutedEventArgs e)
         {
-            if(mqttClient.IsConnected)
+            if (mqttClient.IsConnected)
             {
-                mqttClient.Publish("Application2/Message", Encoding.UTF8.GetBytes(message));
+                mqttClient.Publish("Application1/Message", Encoding.UTF8.GetBytes(message));
             }
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-                    mqttClient = new MqttClient("127.0.0.1");
-                    mqttClient.MqttMsgPublishReceived += publisher;
-                    mqttClient.Subscribe(new string[] { "Application1/Message" }, new byte[] { MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE });
-                    mqttClient.Connect("Application1");           
+            mqttClient = new MqttClient("127.0.0.1");
+            mqttClient.MqttMsgPublishReceived += publisher;
+            mqttClient.Subscribe(new string[] { "Application2/Message" }, new byte[] { MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE });
+            mqttClient.Connect("Application2");
         }
 
         private void publisher(object sender, uPLibrary.Networking.M2Mqtt.Messages.MqttMsgPublishEventArgs e)
